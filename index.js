e36e44edf1b6a3c304b8e7442d238f60c1b6bb1a
gobacktohome <164895243+gobacktohome@users.noreply.github.com>
2024-05-30 15:40:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function showDriveEducate() {
 }
 
 function showLaw() {
-  document.getElementById("incident-status").style.display = "none";
-  document.getElementById("drive-educate").style.display = "none";
-  document.getElementById("law").style.display = "block";
-  document.getElementById("blackbox").style.display = "none";
+    document.getElementById("incident-status").style.display = "none";
+    document.getElementById("drive-educate").style.display = "none";
+    document.getElementById("law").style.display = "block";
+    document.getElementById("blackbox").style.display = "none";
 }
 
 function showBlackbox() {
@@ -32,7 +32,6 @@ function showBlackbox() {
     document.getElementById("law").style.display = "none";
     document.getElementById("blackbox").style.display = "block";
 }
-
 
 const chartData = {
     labels: ["중앙선 침범", "신호위반", "안전거리 미확보", "안전운전 의무 불이행", "교차로 통행방법 위반", "보행자 보호의무 위반", "기타"],
@@ -93,3 +92,41 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll("#introduce img");
+  const texts = [
+    "도로 이용자들 사이에 상호 존중과 안전 문화를 촉진하기 위해 설립된 캠페인입니다. 이 캠페인은 운전자, 자전거 이용자, 보행자 등 모든 도로 사용자들이 서로를 이해하고 배려하는 태도를 기르도록 돕는 것을 목표로 합니다.도로는 모두가 함께 사용하는 공간입니다. 그러나 매년 수많은 사고와 갈등이 발생하면서 도로 안전에 대한 중요성이 커지고 있습니다. Road Respect Campaign은 이러한 문제를 해결하기 위해 다음과 같은 활동을 전개하고 있습니다.",
+    "자료실에서는 사고현황, 교통 안전 법률, 안전교육영상과 같은 컨텐츠들을 제공합니다.", "공모전에서 운전자 또는 도로 관련 공모전을 참여할 수 있습니다.", "퀴즈에서는 영상과 관련된 퀴즈를 풀 수 있습니다.", "후원을 할 수 있습니다."
+  ];
+  const introText = document.getElementById("intro-text");
+  let currentImageIndex = 0;
+  const totalImages = images.length;
+  const displayTime = 10000; // 3 seconds =3000
+
+  function showImage(index) {
+    images.forEach((img, idx) => {
+      img.style.display = idx === index ? "block" : "none";
+    });
+    introText.innerText = texts[index];
+  }
+
+  function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % totalImages;
+    showImage(currentImageIndex);
+  }
+
+  function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + totalImages) % totalImages;
+    showImage(currentImageIndex);
+  }
+
+  document.getElementById("next-btn").addEventListener("click", nextImage);
+  document.getElementById("prev-btn").addEventListener("click", prevImage);
+
+  setInterval(nextImage, displayTime);
+
+  // Show the first image initially
+  showImage(currentImageIndex);
+});
+
